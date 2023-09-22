a65728b85aa8ea8960007d0222b598960efc50df
SabbirSami <smd71430@gmail.com>
2023-09-22 10:55:35 +0600

    set besic style

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -10,7 +10,23 @@ function TicTacToe() {
         <div>
             <div className="container">
                 <h1 className="title">Tic Tac Toe </h1>
-                <div className="board"></div>
+                <div className="board">
+                    <div className="row1">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+                    <div className="row2">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+                    <div className="row3">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+                </div>
                 <button className="reset">Reset</button>
             </div>
             <img src={circleIcon} alt="" />
