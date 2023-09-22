0fcafbce2b5d09fb6e06fa476ac0c18a4eb377ce
SabbirSami <smd71430@gmail.com>
2023-09-22 10:41:40 +0600

    create tic tac toe component and add css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import TicTacToe from "./Components/TicTacToe";
+import "./Style.css";
 function App() {
-    return <></>;
+    return (
+        <div className="tic-tac-tow-body">
+            <TicTacToe />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/TicTacToe.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import circleIcon from "../Assets/circle.png";
+
+TicTacToe.propTypes = {};
+
+function TicTacToe() {
+    return (
+        <div>
+            <img src={circleIcon} alt="" />
+        </div>
+    );
+}
+
+export default TicTacToe;
