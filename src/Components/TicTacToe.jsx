0d619116fcabbde02b45ec73d2207b4981063b14
SabbirSami <smd71430@gmail.com>
2023-09-22 10:48:07 +0600

    add style on reset button

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import circleIcon from "../Assets/circle.png";
+import crossIcon from "../Assets/cross.png";
 
 TicTacToe.propTypes = {};
 
 function TicTacToe() {
     return (
         <div>
+            <div className="container">
+                <h1 className="title">Tic Tac Toe </h1>
+                <div className="board"></div>
+                <button className="reset">Reset</button>
+            </div>
             <img src={circleIcon} alt="" />
         </div>
     );
