3f34ff02fb7a6b4b892865b2715d4619f2d41e7c
SabbirSami <smd71430@gmail.com>
2023-09-22 12:17:36 +0600

    set condition for select winner and show congratulation message

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import { useRef, useState } from "react";
 import circleIcon from "../Assets/circle.png";
 import crossIcon from "../Assets/cross.png";
 
-TicTacToe.propTypes = {};
-
+let data = ["", "", "", "", "", "", "", "", ""];
 function TicTacToe() {
-    let data = ["", "", "", "", "", "", "", "", ""];
     let [count, setCount] = useState(0);
     let [lock, setLock] = useState(false);
+    let titleRef = useRef(null);
+
     const toggle = (e, number) => {
         if (lock) {
             return 0;
@@ -22,11 +21,69 @@ function TicTacToe() {
             data[number] = "o";
             setCount(++count);
         }
+        checkWin();
+    };
+    const checkWin = () => {
+        if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+            won(data[2]);
+        } else if (
+            data[3] === data[4] &&
+            data[4] === data[5] &&
+            data[5] !== ""
+        ) {
+            won(data[5]);
+        } else if (
+            data[6] === data[7] &&
+            data[7] === data[8] &&
+            data[8] !== ""
+        ) {
+            won(data[8]);
+        } else if (
+            data[0] === data[3] &&
+            data[3] === data[6] &&
+            data[6] !== ""
+        ) {
+            won(data[6]);
+        } else if (
+            data[1] === data[4] &&
+            data[4] === data[7] &&
+            data[7] !== ""
+        ) {
+            won(data[7]);
+        } else if (
+            data[2] === data[5] &&
+            data[5] === data[8] &&
+            data[8] !== ""
+        ) {
+            won(data[8]);
+        } else if (
+            data[0] === data[4] &&
+            data[4] === data[8] &&
+            data[8] !== ""
+        ) {
+            won(data[8]);
+        } else if (
+            data[2] === data[4] &&
+            data[4] === data[6] &&
+            data[6] !== ""
+        ) {
+            won(data[6]);
+        }
+    };
+    const won = (winner) => {
+        setLock(true);
+        if (winner === "x") {
+            titleRef.current.innerHTML = `Congratulation: <img src="${crossIcon}" alt="" /> you won the game`;
+        } else {
+            titleRef.current.innerHTML = `Congratulation: <img src="${circleIcon}" alt="" /> you won the game`;
+        }
     };
     return (
         <div>
             <div className="container">
-                <h1 className="title">Tic Tac Toe </h1>
+                <h1 className="title" ref={titleRef}>
+                    Tic Tac Toe{" "}
+                </h1>
                 <div className="board">
                     <div className="row1">
                         <div
